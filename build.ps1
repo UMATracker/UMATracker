1f62ae2129b5827cb5512eee5fc191dd577c1e2d
Licht Takeuchi <licht-t@math.dis.titech.ac.jp>
2016-05-30 03:26:16 +0900

    Small bug fix

--- build.ps1
+++ build.ps1
@@ -1,14 +1,19 @@
 $uma_output_dir=$args[0];
 $current_dir=$PSScriptRoot;
 
-for ( $i = 0; $i -lt 5; $i++ )
-{
-    if(-not((git pull -f) -or (git submodule update --init --recursive)))
+$success = $false;
+do {
+    try
     {
-        break;
+        git pull -f;
+        git submodule update --init --recursive;
+        $success = $true;
     }
-    Start-Sleep -Seconds 60;
-}
+    catch [System.Exception]
+    {
+        Start-Sleep -Seconds 60;
+    }
+} while (!$success);
 
 $list=Get-ChildItem -Path "${current_dir}" -Directory;
 foreach($dir in $list)
