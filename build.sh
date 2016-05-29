f79c2c127892dc8965fc15297f5cfe9e8fe710e9
Licht Takeuchi <licht-t@math.dis.titech.ac.jp>
2016-05-30 00:44:38 +0900

    Added the batch build script for Mac

---
+++ build.sh
@@ -0,0 +1,18 @@
+current_dir=$(pwd)
+
+for entry in "${current_dir}"/*/
+do
+    cd entry
+
+    n=0
+    until [ $n -ge 5 ]
+    do
+        git pull -f && git submodule update --init --recursive && break
+        n=$[$n+1]
+        sleep 60
+    done
+
+    rm -rf ./dist
+    pyinstaller umatracker-mac.spec
+    mv dist/*.app "${output_loc}"
+done
