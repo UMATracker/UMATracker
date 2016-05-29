50a1a0d65942a6d8070a6262894068371e0c7e87
Licht Takeuchi <licht-t@math.dis.titech.ac.jp>
2016-05-30 01:02:29 +0900

    Small bug fix

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 current_dir=$(pwd)
 
+n=0
+until [ $n -ge 5 ]
+do
+    git pull -f && git submodule update --init --recursive && break
+    n=$[$n+1]
+    sleep 60
+done
+
 for entry in "${current_dir}"/*/
 do
     cd ${entry}
-
-    n=0
-    until [ $n -ge 5 ]
-    do
-        git pull -f && git submodule update --init --recursive && break
-        n=$[$n+1]
-        sleep 60
-    done
 
     rm -rf ./dist
     pyinstaller umatracker-mac.spec
